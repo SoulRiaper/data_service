377167372835039d9fcf46b6a4aaeecb104ad9d2
soulriaper <soulriaper@gmail.com>
2024-09-01 18:08:13 +0300

    add docker deploy

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,9 @@
 
 # build environment
-FROM node:18.16.0-alpine as base
+FROM node:18.16.0-alpine
 COPY package.json ./
 RUN yarn install
 COPY src ./src
 COPY tsconfig.json ./tsconfig.json
 RUN npm run build
-
-# run environment
-FROM node:18.16.0-alpine
-
-COPY --from=base ./node_modules ./node_modules
-COPY --from=base ./package.json ./package.json
-COPY --from=base /build /build
-
 CMD ["npm", "run", "start"]
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,42 @@
-import express, { Request, Response} from "express";
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 8080;
 
+const data: { [key: string]: string | string[] } = {
+  bananas: ['123', '321'],
+  mangos: 'i am mango',
+  apples: 'no apples today',
+};
 
 app.get('/', (req: Request, res: Response) => {
-    console.log(`[${new Date().toISOString()}]` ,'user get main');
-    res.status(200).send('hello world');
+  const params = req.query;
+  if (params['query_fields']) {
+    const fields = params['query_fields'];
+    console.log(
+      `[${new Date().toISOString()}]:`,
+      'user search params: ',
+      params,
+    );
+
+    res.json({});
+  } else {
+    res.status(400).send('query must contauin query_fields http param');
+  }
 });
 
-app.listen(port, () => {
+app
+  .listen(port, () => {
     console.log('server running at port: ', port);
-}).on('error', (err) => {
+  })
+  .on('error', (err) => {
     throw new Error(err.message);
-});
+  });
+
+function getDataByParams(params: Array<string>): object {
+  const res: { [key: string]: string | string[] } = {};
+  for (const param of params) {
+    res[param] = data[param];
+  }
+  return res;
+}
